4e1f5aba100dce57a549a182d64fad79b6cb064e
warry258 <95153570+warry258@users.noreply.github.com>
2022-12-04 20:50:00 +0800

    Delete build/k3 directory

--- build/k3/diy-after-compile.sh
+++
@@ -1,3 +0,0 @@
-rm -f $(dirname $(find openwrt/bin/targets/ -type f -name sha256sums ))/openwrt-*-root.squashfs
-
-true
--- build/k3/set_matrix.sh
+++
@@ -1,3 +0,0 @@
-# 要 json raw 字符串
-echo '::set-output name=matrix::[{"name":"lede","branch":"master","addr":"https://github.com/coolsnowwolf/lede"}]'
-# 此步不重要，主要是 custom_pull.sh
