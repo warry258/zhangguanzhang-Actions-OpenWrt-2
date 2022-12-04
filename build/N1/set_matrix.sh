37087c39a9f729eb768096d8db2026b31de951a1
warry258 <95153570+warry258@users.noreply.github.com>
2022-12-04 20:47:30 +0800

    Delete build/N1 directory

--- build/N1/set_matrix.sh
+++
@@ -1,4 +0,0 @@
-# 要 json raw 字符串
-echo '::set-output name=matrix::[{"name":"lede","branch":"master","addr":"https://github.com/coolsnowwolf/lede"}]'
-# 下面日后适配天灵
-# echo '::set-output name=matrix::[{"name":"lede","branch":"master","addr":"https://github.com/coolsnowwolf/lede"},{"name":"immortalwrt","branch":"master","addr":"https://github.com/immortalwrt/immortalwrt"}]'
